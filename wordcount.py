fe78c83feab4d55f271d5b2e4ae6b8a550e33731
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 12:20:35 -0700

    Processed text into list and then dictionary. Stripped text of punctuation.

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,12 @@
 # put your code here.
+text = open("test.txt")
+
+word_count =  {}
+
+for line in text:
+    words = line.rstrip().split(" ")
+    for word in words:
+        word = word.lower().strip(',.!?')
+        word_count[word] = word_count.get(word, 0) + 1
+
+print word_count

c62c3c609041f0f3783a5da8100add59d9395bff
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 12:49:51 -0700

    Prints dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,12 +1,15 @@
 # put your code here.
-text = open("test.txt")
+text = open("twain.txt")
 
 word_count =  {}
 
 for line in text:
     words = line.rstrip().split(" ")
     for word in words:
-        word = word.lower().strip(',.!?')
+        word = word.lower().strip(',.!?:;"_')
         word_count[word] = word_count.get(word, 0) + 1
 
-print word_count
+
+for word, value in word_count.iteritems():
+    print word, value
+#print sorted(word_count.items(), key = lambda x: x[1])
